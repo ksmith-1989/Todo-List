53b47169cd4a2b78fad70a8683fa79254c2e5455
Kyle Smith <kj10068@gmail.com>
2019-07-23 20:07:10 -0700

    much more functional

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a very basic to do list using Javascript. Don't forget to pull up the console on chrome!
+<!-- This is a very basic to do list using Javascript. Don't forget to pull up the console on chrome! -->
+
+No longer using console. More functional.
--- list.js
+++ list.js
@@ -1,34 +1,50 @@
-var todos = ["Buy new Turtle"];
+// This code no longer applies
 
-window.setTimeout(function() {
-  var input = prompt("What would you like to do?");
+// var todos = ["Buy new Turtle"];
 
-  while (input !== "quit") {
-    if (input === "list") {
-      listTodos();
-    } else if (input === "new") {
-      addTodo();
-    } else if (input === "delete") {
-      deleteTodo();
-    }
-    var input = prompt("What would you like to do?");
-  }
-  console.log("OK, YOU QUIT THE APP");
-  function listTodos() {
-    console.log("**********");
-    todos.forEach(function(todo, i) {
-      console.log(i + ": " + todo);
-    });
-    console.log("**********");
-  }
-  function addTodo() {
-    var newTodo = prompt("Enter new todo");
-    todos.push(newTodo);
-    console.log("Added Todo");
-  }
-  function deleteTodo() {
-    var index = prompt("Enter index of todo to delete");
-    todos.splice(index, 1);
-    console.log("Deleted Todo");
-  }
-}, 500);
+// window.setTimeout(function() {
+//   var input = prompt("What would you like to do?");
+
+//   while (input !== "quit") {
+//     if (input === "list") {
+//       listTodos();
+//     } else if (input === "new") {
+//       addTodo();
+//     } else if (input === "delete") {
+//       deleteTodo();
+//     }
+//     var input = prompt("What would you like to do?");
+//   }
+//   console.log("OK, YOU QUIT THE APP");
+//   function listTodos() {
+//     console.log("**********");
+//     todos.forEach(function(todo, i) {
+//       console.log(i + ": " + todo);
+//     });
+//     console.log("**********");
+//   }
+//   function addTodo() {
+//     var newTodo = prompt("Enter new todo");
+//     todos.push(newTodo);
+//     console.log("Added Todo");
+//   }
+//   function deleteTodo() {
+//     var index = prompt("Enter index of todo to delete");
+//     todos.splice(index, 1);
+//     console.log("Deleted Todo");
+//   }
+// }, 500);
+alert("Just click the task when it is completed. EASY!");
+let lis = document.querySelectorAll("li");
+
+for (var i = 0; i < lis.length; i++) {
+  lis[i].addEventListener("mouseover", function() {
+    this.classList.add("selected");
+  });
+  lis[i].addEventListener("mouseout", function() {
+    this.classList.remove("selected");
+  });
+  lis[i].addEventListener("click", function() {
+    this.classList.toggle("done");
+  });
+}
